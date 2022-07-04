fe10ca4754c3b61194a8ea9318178b146dda1b86
NastassiaLoginava <nastik.95@bk.ru>
2022-07-04 20:55:34 +0300

    feat: add API key to the appLoader.js

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9b76fa01398b4fe8b90b10eda302c839', // получите свой ключ https://newsapi.org/
         });
     }
 }
